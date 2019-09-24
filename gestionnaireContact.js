eb56ae5313b04d7c6655729f6b92efcd92ed3bb8
Ella <ella.demange@gmail.com>
2019-09-24 18:54:15 +0200

    Tableau de contacts

--- gestionnaireContact.js
+++ gestionnaireContact.js
@@ -149,6 +149,69 @@ var app2 = new Vue({ // affiche les contacts sous forme de liste et permet la re
 
 
 
+// À nettoyer !!! 
+Vue.component('demo-grid', {
+  template: '#grid-template',
+  props: {
+    heroes: Array,
+    columns: Array,
+    filterKey: String
+  },
+  data: function () {
+    var sortOrders = {}
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1
+    })
+    return {
+      sortKey: '',
+      sortOrders: sortOrders
+    }
+  },
+  computed: {
+    filteredHeroes: function () {
+      var sortKey = this.sortKey
+      var filterKey = this.filterKey && this.filterKey.toLowerCase()
+      var order = this.sortOrders[sortKey] || 1
+      var heroes = this.heroes
+      if (filterKey) {
+        heroes = heroes.filter(function (row) {
+          return Object.keys(row).some(function (key) {
+            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+          })
+        })
+      }
+      if (sortKey) {
+        heroes = heroes.slice().sort(function (a, b) {
+          a = a[sortKey]
+          b = b[sortKey]
+          return (a === b ? 0 : a > b ? 1 : -1) * order
+        })
+      }
+      return heroes
+    }
+  },
+  filters: {
+    capitalize: function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
+  }
+})
+
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    searchQuery: '',
+    gridColumns: [ 'name', 'surname', 'mobilePhone', 'phoneNumber', ''],
+    gridData: contactsList 
+  }
+})
+
 
 // A améliorer : suppression un peu lente sur liste avec beaucoup de noms et pas de réinitialisation des champs "ajouter contact" après un nouvel ajout 
 
